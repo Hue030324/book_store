949821a3e3549b1e29624da8e69c6a14500c1ac6
Hue030324 <hue030304@gmail.com>
2023-11-14 00:54:48 +0700

    update

--- admin/admin_home.php
+++ admin/admin_home.php
@@ -1,3 +1,17 @@
+<?php
+
+include 'connect.php';
+
+session_start();
+
+$admin_id = $_SESSION['admin_id'];
+
+if(!isset($admin_id)){
+   header('location:?act=home');
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,7 +29,7 @@
 
         <div class="content">
             <h3>hi, <span>admin</span></h3>
-            <h1>welcom <span></span></h1>
+            <h1>welcom <span><?php echo $_SESSION['admin_name']; ?></span></h1>
             <p>this is admin page</p>
             <a href="../login.php" class="btn">login</a>
             <a href="../resgister.php" class="btn">register</a>
